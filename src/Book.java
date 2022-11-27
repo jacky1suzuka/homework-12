53e5ba8e0127d480a5c05b5eef8658660ec5e31f
Eugene <jacky1suzuka@gmail.com>
2022-11-27 18:44:19 +0300

    Check HW

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,32 @@
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
     private int yearPublishing;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublishing == book.yearPublishing && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, yearPublishing);
+    }
+
     public Book(String name, Author author, int yearPublishing) {
         this.name = name;
         this.author = author;
         this.yearPublishing = yearPublishing;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: " + name + "; Автор: " + author.toString() + "; Год публикации: " + yearPublishing;
     }
 
     public String getName() {
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         Author author = new Author("Lev", "Tolstoy");
-        Author author1 = new Author("George", "Orwell");
+        Author author1 = new Author("Lev", "Tolstoy");
 
         Book book = new Book("Anna Karenina", author, 1878);
         Book book1 = new Book("1984", author1, 1949);
 
-        book.setYearPublishing(1950);
+        book1.setYearPublishing(1950);
         System.out.println(book1.getAuthor());
         System.out.println(book.getAuthor());
+        System.out.println(book1);
+        System.out.println(book.equals(book1));
+        System.out.println(author.equals(author1));
+        System.out.println(book.hashCode());
 
     }
 }
